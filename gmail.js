38dfe9f03cd99a8fc1b8096d0f6b8e7f14ec97ad
streetturtle <streetturtle@users.noreply.github.com>
2018-06-07 08:18:12 -0400

    Update gmail.js

--- gmail.js
+++ gmail.js
@@ -1,1 +1,22 @@
-document.querySelector(".aKB").style.display="none";
+function applycss(css){
+    var head = document.getElementsByTagName('head')[0];
+    var s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    s.appendChild(document.createTextNode(css));
+    head.appendChild(s);
+ }
+applycss(`
+
+  .aKB{
+    display:none;
+  }
+  .bsU{
+      border: 2px solid teal;
+      border-radius: 8px;
+      font-weight: bold;
+      text-align: center;
+      padding: 0 !important;
+      width: 20px;
+  }
+
+`);
