2b7bf004f5eacca71161d9d9cc0b4bea1ee83dac
AXeL-dev <contact.axel.dev@gmail.com>
2019-03-13 17:49:04 +0100

    add js for gmail & outlook unread messages count

---
+++ gmail-unread-count.js
@@ -0,0 +1,12 @@
+function checkUnread() {
+    var e = document.getElementsByClassName("bsU");
+    var t = 0;
+    for( i = 0; i < e.length; i++ ) t += parseInt(e[i].innerHTML.trim());
+    updateBadge(t);
+}
+
+function updateBadge(e) {
+	e && e >= 1 ? rambox.setUnreadCount(e) : rambox.clearUnreadCount();
+}
+
+setInterval(checkUnread, 3000);
