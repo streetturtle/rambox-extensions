f4846d208ae4532e58651900dba34b2d4a9e89f9
Raul Dipeas <rauldipeas@mail.com>
2019-06-26 12:23:18 -0300

    Update spike-dark.js and README.MD with Spike Dark screenshot #3

--- spike-dark.js
+++ spike-dark.js
@@ -27,8 +27,14 @@ img.icon {filter: invert(0%)}
 .emojiCategory {filter: invert(100%)}
 .emojiCategory.emojiCategory {filter: invert(100%)}
 .emojiCategory.selected {filter: invert(100%)}
+.hover {filter: invert(100%)}
+.hover:hover {background-color: #0085ff}
 .on {filter: invert(100%)}
 .person .name {filter: invert(100%)}
+.section {filter: invert(0%)}
+.section:hover {filter: invert(100%);}
+.section:hover {background-color: #0085ff}
+.section:hover .title {filter: invert(100%)}
 .separatorBottom div {filter: invert(100%)}
 .separatorBottom .picture-border {filter: invert(100%)}
 .separatorBottom .title {filter: invert(0%)}
