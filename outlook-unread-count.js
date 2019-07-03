840aa2cfd4f363b374f28a841ae1771bfb0800f1
Raul Dipeas <rauldipeas@mail.com>
2019-07-03 12:34:22 -0300

    Rollback Outlook unread count

--- outlook-unread-count.js
+++ outlook-unread-count.js
@@ -1,10 +1,12 @@
 function checkUnread() {
-    var e = null !== $(".ms-FocusZone [role=tree]:last i[data-icon-name=Inbox]").siblings()[1].querySelector("span span") ? $(".ms-FocusZone [role=tree]:last i[data-icon-name=Inbox]").siblings()[1].querySelector("span span").innerText : 0;
-    updateBadge("" === e ? 0 : parseInt(e));
+    var elem = document.querySelector('i[data-icon-name="Inbox"]+span+span>span');
+    var count = elem ? elem.innerText.split('\n')[0] : 0;
+    var t = parseInt(count);
+    updateBadge(t);
 }
 
 function updateBadge(e) {
-    1 <= e ? rambox.setUnreadCount(e) : rambox.clearUnreadCount();
+    e && e >= 1 ? rambox.setUnreadCount(e) : rambox.clearUnreadCount();
 }
 
 setInterval(checkUnread, 3000);
